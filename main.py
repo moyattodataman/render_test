5cec8bf50ed664a6eb5dbeab3532f1b360274a1b
モヤっとデータサイエンティスト <107286388+moyattodataman@users.noreply.github.com>
2023-02-20 01:38:38 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# "/"にGETメソッドでリクエストした際の処理を定義
 @app.get("/")
 def index():
-    return {"Hello": "ahhhhh!"}
+    return {"Hello": "World"}
+
+# "/items/{item_id}"にGETメソッドでリクエストした際の処理を定義
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
